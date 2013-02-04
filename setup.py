0a74a380f505a01da9bcfb230f61eada6c80a0df
Hasnain Lakhani <m.hasnain.lakhani@gmail.com>
2013-02-04 19:10:42 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+    name = "Halwa",
+    version = "0.1",
+    py_modules = ['halwa'],
+
+    install_requires = ['Hamlish-Jinja>=0.3.0','Jinja2>=2.6', 'Markdown>=2.2.1'],
+
+    package_data = {
+        '': ['*.md'],
+    },
+    zip_safe = True,
+
+    # metadata for upload to PyPI
+    author = "Hasnain Lakhani",
+    author_email = "m.hasnain.lakhani@gmail.com",
+    description = "Single file static site generator.",
+    keywords = "static site generator",
+    url = "https://github.com/mhlakhani/halwa",
+    download_url = "git+https://github.com/mhlakhani/halwa.git"
+
+)
